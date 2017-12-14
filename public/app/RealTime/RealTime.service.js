68f8d784ea910bc4def9480707514625c5ae9bc2
serp-ya <sergei.kundryukov1989@gmail.com>
2017-12-14 16:31:39 +0300

    Переопределил порт Ангулар Вебсокет

--- public/app/RealTime/RealTime.service.js
+++ public/app/RealTime/RealTime.service.js
@@ -1,6 +1,6 @@
 angular.module('RealTime')
   .factory('RealTimeService', function (socketFactory) {
-    const myIoSocket = io.connect(':3000');
+    const myIoSocket = io.connect();
 
     mySocket = socketFactory({
       ioSocket: myIoSocket
