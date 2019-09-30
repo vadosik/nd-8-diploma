47a6aedee737eb1bdbd2a92b7371dc241d814d21
vadosik <SsGg56@mail.ru>
2019-09-30 21:20:23 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: process.env.MONGO_DB_URL  || 'mongodb+srv://test:test@mig-cafe-lypoi.mongodb.net/test?retryWrites=true&w=majority'
+  url: process.env.MONGO_DB_URL  || 'mongodb://test:<password>@mig-cafe-shard-00-00-lypoi.mongodb.net:27017,mig-cafe-shard-00-01-lypoi.mongodb.net:27017,mig-cafe-shard-00-02-lypoi.mongodb.net:27017/test?ssl=true&replicaSet=mig-cafe-shard-0&authSource=admin&retryWrites=true&w=majority'
 };
